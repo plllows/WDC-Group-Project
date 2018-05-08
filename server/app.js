47b018399616b682e0fab03df01baf9c80b633dc
Yuanjing Zhao <zhaoyuanjing1999@icloud.com>
2018-05-08 10:34:02 +0930

    created fixed sign in form

--- server/app.js
+++ server/app.js
@@ -4,6 +4,8 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -20,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({secret: 'HUEHUEHUEHUE', resave: false, saveUninitialized: true,}));
 
 app.use('/', routes);
 app.use('/users', users);
