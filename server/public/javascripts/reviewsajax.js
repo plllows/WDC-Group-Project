daa272abce319f88f29d205ec3b910e2920307b2
AndyClo <37825584+AndyClo@users.noreply.github.com>
2018-05-10 19:06:21 +0930

    Add files via upload

---
+++ server/public/javascripts/reviewsajax.js
@@ -0,0 +1,22 @@
+function loadXMLDoc()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+	
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			document.getElementById("myDiv").innerHTML=xmlhttp.responseText;
+		}
+	}
+	xmlhttp.open("GET","data/reviews.txt",true);
+	xmlhttp.send();
+}
